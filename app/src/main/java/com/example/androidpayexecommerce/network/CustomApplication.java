abfa9e8274c6ee2c73653dfa809c782e9e251130
pramod kumar <kumarpramod559@gmail.com>
2019-04-19 18:45:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidpayexecommerce/network/CustomApplication.java
@@ -0,0 +1,20 @@
+package com.example.androidpayexecommerce.network;
+
+import android.app.Application;
+
+        import com.android.volley.RequestQueue;
+
+public class CustomApplication extends Application{
+
+    private RequestQueue requestQueue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        requestQueue = VolleySingleton.getInstance(getApplicationContext()).getRequestQueue();
+    }
+
+    public RequestQueue getVolleyRequestQueue(){
+        return requestQueue;
+    }
+}
